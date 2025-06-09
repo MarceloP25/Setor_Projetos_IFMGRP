c495ef99d25394698634f89e0b5301c4c1394339
Marcelo P. Patrício <marc3lo.patricio.25@gmail.com>
2025-06-09 14:46:47 -0300

    Inicio atualizações e padronização

--- project_management/src/components/Header/index.tsx
+++ project_management/src/components/Header/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header: React.FC = () => (
   <div className="header">
-    <h1>Setor de Projetos IFSMG - RP</h1>
+    <h1>Setor de Projetos IFMG - RP</h1>
   </div>
 );
 
